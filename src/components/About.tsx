e82c88c235e6df773c42e2ecf9e60c98298eccf2
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-04-23 12:57:14 -0700

    WIP add animated bubbles to background

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="absolute inset-0">
             <div className="flex justify-center items-center text-center flex-col relative z-30 mx-auto mt-9 sm:mt-20 md:mt-24 w-[90%] sm:w-[85%] md:w-[80%] lg:w-[75%] xl:w-[75%] xl:max-w-[1145px]   bg-custom-gradient-3 rounded-xl text-white font-heading py-8 sm:py-14 md:py-20 gap-5 sm:gap-8">
-                <div className="h-[200px] md:h-[230px] lg:h-[260px] xl:h-[290px] w-[200px] md:w-[230px] lg:w-[260px] xl:w-[290px] rounded-full border-4 border-coral overflow-hidden">
+                <div className="h-[200px] md:h-[230px] lg:h-[260px] xl:h-[290px] w-[200px] md:w-[230px] lg:w-[260px] xl:w-[290px] rounded-full border-4 border-[#4865FD] overflow-hidden">
                     <img
                         src="./kaitlin-lovrich-profile-picture.png"
                         alt="kaitlin lovrich profile picture"
--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,39 +1,169 @@
 export default function Background() {
     return (
-        <div className="">
-            <div className="relative w-full z-0 h-lvh">
-                <img
-                    src="./purple-spongebob-flower.png"
-                    alt="purple spongebob flower"
-                    className="absolute top-[-100px] sm:top-[-110px] left-[-110px] sm:left-[-113px] max-w-[385px] sm:max-w-[455px] lg:max-w-[485px]"
-                />
+        <>
+            <Flowers />
+            <Bubbles />
+        </>
+    );
+}
 
-                <img
-                    src="./pink-spongebob-flower.png"
-                    alt="pink spongebob flower"
-                    className="absolute top-[25px] left-[205px] sm:left-[245px] lg:left-[290px] max-w-[175px] sm:max-w-[245px] lg:max-w-[275px]"
-                />
-                <img
-                    src="./white-spongebob-flower.png"
-                    alt="white spongebob flower"
-                    className="absolute top-[250px] sm:top-[320px] left-[0px] sm:left-[-5px] max-w-[85px] sm:max-w-[135px] lg:max-w-[165px]"
-                />
-                <img
-                    src="./yellow-spongebob-flower.png"
-                    alt="yellow spongebob flower"
-                    className="absolute top-[200px] md:top-[15px] right-[-7px] sm:right-[0px] max-w-[245px] sm:max-w-[315px] lg:max-w-[375px]"
-                />
-                <img
-                    src="./coral-spongebob-flower.png"
-                    alt="coral spongebob flower"
-                    className="absolute bottom-[55px] sm:bottom-[40px] left-[-100px] sm:left-[-45px] lg:left-[10px] max-w-[265px] sm:max-w-[335px] lg:max-w-[365px]"
-                />
-                <img
-                    src="./blue-spongebob-flower.png"
-                    alt="blue spongebob flower"
-                    className="absolute bottom-[95px] sm:bottom-[110px] right-[35px] md:right-[90px] max-w-[135px] sm:max-w-[205px] lg:max-w-[265px]"
-                />
+function Flowers() {
+    return (
+        <div className="relative w-full z-0 h-lvh">
+            <img
+                src="./purple-spongebob-flower.png"
+                alt="purple spongebob flower"
+                className="absolute top-[-100px] sm:top-[-110px] left-[-110px] sm:left-[-113px] max-w-[385px] sm:max-w-[455px] lg:max-w-[485px]"
+            />
+
+            <img
+                src="./pink-spongebob-flower.png"
+                alt="pink spongebob flower"
+                className="absolute top-[-7px] sm:top-[0px] md:top-[25px] left-[205px] sm:left-[265px] lg:left-[290px] max-w-[175px] sm:max-w-[245px] lg:max-w-[275px]"
+            />
+            <img
+                src="./white-spongebob-flower.png"
+                alt="white spongebob flower"
+                className="absolute top-[250px] sm:top-[320px] left-[0px] sm:left-[-5px] max-w-[85px] sm:max-w-[135px] lg:max-w-[165px]"
+            />
+            <img
+                src="./yellow-spongebob-flower.png"
+                alt="yellow spongebob flower"
+                className="absolute top-[200px] md:top-[15px] right-[-7px] sm:right-[0px] max-w-[245px] sm:max-w-[315px] lg:max-w-[375px]"
+            />
+            <img
+                src="./coral-spongebob-flower.png"
+                alt="coral spongebob flower"
+                className="absolute bottom-[0px] sm:bottom-[40px] left-[-100px] sm:left-[-45px] lg:left-[10px] max-w-[265px] sm:max-w-[335px] lg:max-w-[365px]"
+            />
+            <img
+                src="./blue-spongebob-flower.png"
+                alt="blue spongebob flower"
+                className="absolute bottom-[0px] sm:bottom-[110px] right-[0px] sm:right-[35px] md:right-[90px] max-w-[135px] sm:max-w-[205px] lg:max-w-[265px]"
+            />
+        </div>
+    );
+}
+
+function Bubbles() {
+    return (
+        <div className="absolute inset-0">
+            <div className="relative w-full z-10 h-lvh">
+                <div className="absolute top-[-199px] sm:top-[-223px] md:top-[-210px] left-[229px] sm:left-[433px] md:left-[484px] lg:left-[530px]">
+                    <LargeBubble />
+                </div>
+                <div className="absolute top-[-261px] sm:top-[-230px] right-[6px] sm:right-[25px] lg:right-[100px]">
+                    <LargeBubble />
+                </div>
+                <div className="absolute top-[-190px] lg:top-[-180px] left-[170px] sm:left-[220px] lg:left-[240px]">
+                    <MediumBubble />
+                </div>
+                <div className="absolute top-[-98px] sm:top-[-80px] lg:top-[-65px] left-[-29px] sm:left-[15px] lg:left-[30px]">
+                    <MediumBubble />
+                </div>
+                <div className="absolute hidden md:block md:top-[-90px] lg:top-[-135px] md:right-[145px] lg:right-[200px]">
+                    <MediumBubble />
+                </div>
+                <div className="absolute top-[65px] right-[5px]">
+                    <MediumBubble />
+                </div>
+                <div className="absolute top-[375px] right-[0px] sm:right-[30px] lg:right-[60px]">
+                    <MediumBubble />
+                </div>
+                <div className="absolute hidden sm:block sm:top-[565px] sm:right-[18px] lg:right-[30px]">
+                    <MediumBubble />
+                </div>
+                <div className="absolute top-[70px] sm:top-[80px] lg:top-[188px] left-[-5px] sm:left-[43px] lg:left-[90px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[-10px] sm:top-[197px] lg:top-[212px] left-[172px] sm:left-[4px] lg:left-[5px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[-70px] sm:top-[-53px] lg:top-[-43px] left-[158px] sm:left-[205px] lg:left-[225px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[-283px] lg:top-[19px] left-[0px] sm:left-[732px] lg:left-[732px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[-144px] sm:top-[-148px] left-[182px] sm:left-[481px] md:left-[532px] lg:left-[578px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[-51px] sm:top-[16px] right-[0px] sm:right-[15px] lg:right-[96px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[207px] right-[0px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[422px] right-[0px] sm:right-[35px] lg:right-[65px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[517px] right-[0px] sm:right-[25px] lg:right-[55px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute hidden sm:block sm:top-[722px] sm:right-[35px] lg:right-[45px]">
+                    <SmallBubble />
+                </div>
+                <div className="absolute top-[-45px] sm:top-[-42px] left-[170px] sm:left-[473px] md:left-[524px] lg:left-[570px]">
+                    <XSmallBubble />
+                </div>
+
+                <div className="absolute top-[38px] sm:top-[73px] right-[5px] sm:right-[21px] lg:right-[106px]">
+                    <XSmallBubble />
+                </div>
+                <div className="absolute top-[297px] right-[8px] lg:right-[8px]">
+                    <XSmallBubble />
+                </div>
+                <div className="absolute top-[376px] right-[2px] sm:right-[19px] lg:right-[32px]">
+                    <XSmallBubble />
+                </div>
+                <div className="absolute top-[608px] sm:top-[663px] right-[15px] lg:right-[35px]">
+                    <XSmallBubble />
+                </div>
+                <div className="absolute hidden sm:block sm:top-[838px] sm:right-[10px]">
+                    <XSmallBubble />
+                </div>
+                <div className="absolute top-[153px] right-[0px] sm:right-[7px]">
+                    <XSmallBubble />
+                </div>
             </div>
         </div>
     );
 }
+
+function LargeBubble() {
+    return (
+        <div className="flex relative bubble w-[45px] h-[37px] border-[3px] m-2 text-xl font-bold font-special">
+            <p className="absolute top-[-8px] right-[8px] rotate-[-66deg]">
+                ){" "}
+            </p>
+        </div>
+    );
+}
+
+function MediumBubble() {
+    return (
+        <div className="flex relative bubble w-[35px] h-[27px] border-[2px] m-2 text-base font-bold font-special">
+            <p className="absolute top-[-6px] sm:top-[-8px] right-[6px] sm:right-[5px] rotate-[-66deg]">
+                ){" "}
+            </p>
+        </div>
+    );
+}
+
+function SmallBubble() {
+    return (
+        <div className="flex bubble w-[18px] h-[14px] border-[2px] m-2 text-xl font-bold font-special"></div>
+    );
+}
+
+function XSmallBubble() {
+    return (
+        <div className="flex bubble w-[8px] h-[6px] border-[1px] m-2 text-xl font-bold font-special"></div>
+    );
+}
+
+// After animation subtract
+// -250px from top for large bubbles
+// -195px from top for med bubbles
+// -93px from top for small bubbles
+// -37 from top for xs bubbles
