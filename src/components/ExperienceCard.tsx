001aa2ac35bfa2f80dee937719d2ce08dc7723da
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-06-07 12:46:51 -0700

    Remove scroll snap effect

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -1,6 +1,6 @@
 import { ExperienceCardProps } from "../lib/types";
 
-export default function ProjectCard({ experience }: ExperienceCardProps) {
+export default function ExperienceCard({ experience }: ExperienceCardProps) {
     const {
         imageSrc,
         altText,
@@ -11,7 +11,7 @@ export default function ProjectCard({ experience }: ExperienceCardProps) {
         experienceBulletPoints,
     } = experience;
     return (
-        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start gap-2 md:gap-6 bg-white/15 rounded-lg p-4 shadow-textBox w-full snap-start first:scroll-mb-96 scroll-mt-6">
+        <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start gap-2 md:gap-6 bg-white/15 rounded-lg p-4 shadow-textBox w-full">
             <div className={imageWidth}>
                 <img
                     src={imageSrc}
@@ -21,7 +21,7 @@ export default function ProjectCard({ experience }: ExperienceCardProps) {
             </div>
             <div className="w-90% lg:w-3/4 ml-8 sm:ml-4">
                 <h2 className="font-heading text-2xl">{experienceTitle}</h2>
-                <div className="flex flex-col-reverse min-[1300px]:flex-row justify-between font-semibold  gap-1 min-[1300px]:gap-20  pt-1.5 pb-3 italic">
+                <div className="flex flex-col-reverse min-[1300px]:flex-row justify-between font-semibold gap-1 min-[1300px]:gap-20 pt-1.5 pb-3 italic">
                     <p className="max-w-[327px]">{experienceLocation}</p>
                     <p className="min-w-[260px] min-[1300px]:text-end">
                         {experienceDate}
