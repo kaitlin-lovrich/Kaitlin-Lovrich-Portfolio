16eb48c0a71d830203c78e29dbb939da5d7740a0
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-08-15 17:21:11 -0700

    Edited experience

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -46,9 +46,9 @@ export const experiencesData: experienceDataType[] = [
         experienceLocation: "Bullies BBQ - Irvine, CA",
         experienceDate: "February 2024 - March 2024",
         experienceBulletPoints: [
-            "Collaborated with 2 developers to build the company's E-Commerce web application in 7 weeks",
+            "Collaborated with 2 developers to build Bullies BBQ's E-Commerce web application in under 8 weeks, utilizing Vite's build system for rapid development and Git for efficient version control and collaboration",
             "Utilized Next.js for optimizing page loading efficiency, leveraging its server-side rendering, automatic code splitting, and built-in routing for maximum speed and scalability in the React project's architecture",
-            "Maintained cohesive styling across web pages and implemented semantic HTML for improved SEO",
+            "Maintained cohesive and responsive styling across all web pages using TailwindCSS, while implementing semantic HTML elements to enhance accessibility and improve SEO performance",
             "Interfaced with company stakeholders in bi-weekly meetings to discuss software needs and progress",
         ],
     },
@@ -72,9 +72,10 @@ export const experiencesData: experienceDataType[] = [
         experienceLocation: "Career Break - Irvine, CA",
         experienceDate: "May 2022 - Aug 2023",
         experienceBulletPoints: [
-            "After concluding a successful career as a Hairdresser at Halo for Men in May 2022, I took a deliberate and self-directed career shift to pursue my passion for programming. During this period, I dedicated myself to learning coding full-time, primarily utilizing Codecademy.com. I completed eight comprehensive courses, gaining proficiency in various programming languages and frameworks.",
-            "My self-learning journey extended beyond online courses as I actively engaged with the programming community on platforms like Stack Overflow and extensively read documentation to deepen my understanding. This immersive learning experience allowed me to develop practical skills, evident through the completion of several mini-projects and hands-on work on a Python project utilizing Django and Handlebars.",
-            "I believe this self-directed learning period has not only expanded my technical skills but has also enhanced my problem-solving abilities and adaptability to new challenges.",
+            "After concluding a successful career as a Hairdresser, I transitioned to programming, dedicating myself full-time to learning to code. I completed 16 courses through Codecademy, covering various programming languages and frameworks before applying to the LearningFuze Web Development bootcamp in Irvine, California",
+            "Capstone project: Developed Django Delights, an inventory management application using Python, Django, Handlebars, and CSS. The app features JavaScript event listeners for dynamic database interactions, allowing users to efficiently track and manage food inventory",
+            "Python Mini Projects: Created several mini-projects, including a command-line battleship game, a Scrabble word game, a trip calculator, and a program to manage business, franchise, and menu profiles",
+            "Additional Learning: Supplemented CodeCademy courses with MDN documentation, Stack Overflow, and W3Schools",
         ],
     },
     {
@@ -86,6 +87,7 @@ export const experiencesData: experienceDataType[] = [
             "Halo For Men (Lady Janes's Haircuts for Men, Sport Clips) - Chicago, IL",
         experienceDate: "May 2016 - May 2022",
         experienceBulletPoints: [
+            "Managed and streamlined both online and in-person bookings effectively through Book4Time POS software, gaining familiarity with software tools and technologies that support business operations",
             "Built and maintained client relationships, driving repeat business and surpassing sales goals by 30%",
             "Resolved client issues professionally, ensuring 100% satisfaction through effective communication",
             "Ensured punctual store openings and managed backend operations for 40 daily bookings",
