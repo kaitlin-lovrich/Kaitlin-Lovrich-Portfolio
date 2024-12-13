49b6fd1ae02b2c8239547f89593b2ec378de855f
Kaitlin Lovrich <kaylovrich@gmail.com>
2024-12-13 13:54:46 -0800

    Edit project card format and download resume button

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TbHeartCode } from "react-icons/tb";
 import { ProjectCardProps } from "../lib/types";
 
 export default function ProjectCard({ project }: ProjectCardProps) {
-    const [showViewSourceCode, setShowViewSourceCode] = useState(false);
-    const [showViewLive, setShowViewLive] = useState(false);
+    const [showOptions, setShowOptions] = useState(false);
     const [isImageLoaded, setIsImageLoaded] = useState(false);
+    const containerRef = useRef<HTMLElement>(null);
+
+    const handleTouchOutside = (event: TouchEvent) => {
+        console.log(event);
+        if (
+            containerRef.current &&
+            !containerRef.current.contains(event.target as Node)
+        ) {
+            setShowOptions(false); // Hide options when touching outside
+        }
+    };
+
+    useEffect(() => {
+        if (showOptions) {
+            document.addEventListener("touchstart", handleTouchOutside);
+        } else {
+            document.removeEventListener("touchstart", handleTouchOutside);
+        }
+
+        // Cleanup the listener when the component unmounts or options are hidden
+        return () => {
+            document.removeEventListener("touchstart", handleTouchOutside);
+        };
+    }, [showOptions]);
 
     const {
         imageSrc,
@@ -25,14 +48,9 @@ export default function ProjectCard({ project }: ProjectCardProps) {
                 isImageLoaded ? "opacity-100 scale-100" : "opacity-0 scale-95"
             }`}
         >
-            <a
-                href={projectURL}
-                target="_blank"
+            <figure
                 className="relative group w-full"
-                onMouseEnter={() => setShowViewLive(true)}
-                onMouseLeave={() => setShowViewLive(false)}
-                onTouchStart={() => setShowViewLive(true)}
-                onTouchEnd={() => setShowViewLive(false)}
+                onTouchStart={() => setShowOptions(true)}
             >
                 <img
                     src={imageSrc}
@@ -41,59 +59,64 @@ export default function ProjectCard({ project }: ProjectCardProps) {
                     onLoad={() => setIsImageLoaded(true)}
                 />
                 <figcaption
+                    ref={containerRef}
+                    onClick={() => setShowOptions(true)}
+                    onMouseLeave={() => setShowOptions(false)}
                     className={`absolute inset-0 flex items-center justify-center bg-custom-gradient-4 opacity-0 transition-opacity group-hover:opacity-100 ease-in-out duration-500 ${
-                        showViewLive
-                            ? "opacity-100 ease-in-out duration-500"
+                        showOptions
+                            ? "ease-in-out duration-500 opacity-100"
                             : ""
                     }`}
                 >
-                    <span className="font-heading text-4xl">View Live</span>
+                    <div className="flex flex-col justify-evenly text-center font-heading text-3xl h-2/3">
+                        <div className="relative w-[138px] xl:w-[151px] h-[42px] self-center *:hover:scale-105 *:transition *:transform *:duration-300 *:ease-in-out">
+                            <a
+                                href={projectURL}
+                                target="_blank"
+                                aria-label="View Live Website"
+                                className="flex justify-center relative z-10 w-[136px] xl:w-[154px] px-5 xl:px-6 py-1.5 xl:py-2 border-2 border-white/85 hover:border-sky-blue rounded-full text-base xl:text-lg font-heading"
+                            >
+                                VIEW LIVE
+                            </a>
+                            <div className="w-[88%] xl:w-[92%] h-[70%] xl:h-[88%] top-[-3px] left-[3px] absolute m-2 bg-bubble-gradient blur-[4px] rounded-full"></div>
+                        </div>
+                        <div className="relative w-[176px] xl:w-[192px] h-[42px] self-center *:hover:scale-105 *:transition *:transform *:duration-300 *:ease-in-out">
+                            <a
+                                href={projectSourceCode}
+                                target="_blank"
+                                aria-label="View Source Code"
+                                className="flex justify-center relative z-10 w-[170px] xl:w-[192px] px-5 xl:px-6 py-1.5 xl:py-2 border-2 border-white/85 hover:border-sky-blue rounded-full text-base xl:text-lg font-heading"
+                            >
+                                SOURCE CODE
+                            </a>
+                            <div className="w-[88%] xl:w-[92%] h-[70%] xl:h-[88%] top-[-3px] left-[3px] absolute m-2 bg-bubble-gradient blur-[4px] rounded-full"></div>
+                        </div>
+                    </div>
                 </figcaption>
-            </a>
+            </figure>
 
             <div className="flex flex-col gap-2 px-1.5 min-[1415px]:px-3 py-4">
-                <a
-                    href={projectSourceCode}
-                    target="_blank"
-                    aria-label="View Source Code"
-                    className="flex justify-between hover:cursor-pointer"
-                    onMouseEnter={() => setShowViewSourceCode(true)}
-                    onMouseLeave={() => setShowViewSourceCode(false)}
-                    onTouchStart={() => setShowViewSourceCode(true)}
-                    onTouchEnd={() => setShowViewSourceCode(false)}
-                    onClick={() => setShowViewSourceCode(true)}
-                >
-                    <div className="relative flex-auto font-heading text-xl xl:text-2xl">
-                        <h2
-                            className={`absolute transition transform ease-in-out duration-500 ${
-                                showViewSourceCode
-                                    ? "opacity-0 scale-50"
-                                    : "opacity-100 scale-100"
-                            }`}
-                        >
+                <div className="flex justify-between">
+                    <div className="flex-auto">
+                        <h2 className="font-heading text-xl xl:text-2xl">
                             {projectName}
                         </h2>
-
-                        <h2
-                            className={`absolute transition transform ease-in-out duration-500 ${
-                                showViewSourceCode
-                                    ? "opacity-100 scale-100 drop-shadow-custom-blue"
-                                    : "opacity-0 scale-50"
-                            }`}
-                        >
-                            View Source Code
-                        </h2>
                     </div>
-                    <span
-                        className={`self-center text-3xl md:text-4xl xl:text-5xl duration-500 ease-in-out ${
-                            showViewSourceCode
-                                ? "scale-110 drop-shadow-custom-blue"
-                                : "custom-pulse"
-                        }`}
-                    >
-                        <TbHeartCode />
-                    </span>
-                </a>
+                    <div className="relative group">
+                        <a
+                            href={projectSourceCode}
+                            target="_blank"
+                            aria-label="View Source Code"
+                            className="self-center text-3xl md:text-4xl xl:text-5xl duration-500 ease-in-out hover:scale-110 hover:drop-shadow-custom-blue custom-pulse"
+                        >
+                            <TbHeartCode />
+                        </a>
+                        <div className="absolute z-50 hidden group-hover:block bg-blue-950 bg-opacity-100 w-[145px] text-white text-sm rounded px-3 py-1 -top-full">
+                            View source code on Github
+                        </div>
+                    </div>
+                </div>
+
                 <div className="font-bold">
                     <p>{projectType}</p>
                     <p>{projectRole}</p>
